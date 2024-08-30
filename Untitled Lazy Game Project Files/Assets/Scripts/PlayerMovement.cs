bb707c57f794b8390988bc385835d83c89903381
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-08-30 20:28:24 +0200

    pain

--- Untitled Lazy Game Project Files/Assets/Scripts/PlayerMovement.cs
+++ Untitled Lazy Game Project Files/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb; //the rigidbody attached to the player
     public bool isGrounded = true; //check if player is grounded
+    public bool isWalled = false;
+
+    public bool isWallLeft = false;
+    public bool isWallRight = false;
+
     public Transform orientation; //check the direction the player is facing
     public float moveForce; //move the player to the right in air and ground
     public float upForce; //move the player to the left in air and ground
+    public float sideForce;
 
     public SpriteRenderer spriteRenderer;
 
@@ -26,9 +32,17 @@ public class PlayerMovement : MonoBehaviour
             spriteRenderer.flipX = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
+            if (isGrounded == true)
+            {
+                Jump();
+            }
+            
+            if(isWalled == true)
+            {
+                WallJump();
+            }
         }
 
     }
@@ -38,11 +52,37 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(orientation.up * upForce, ForceMode2D.Impulse);
     }
 
+    void WallJump()
+    {
+        if (isWallLeft == true)
+        {
+            rb.AddForce(-orientation.right * sideForce, ForceMode2D.Impulse);
+        }
+
+        if(isWallRight == true)
+        {
+            rb.AddForce(orientation.right * sideForce, ForceMode2D.Impulse);
+        }
+    }
+
     public void SetGrounded(bool state) //this is used to allow me to check for grounded in a collsion script
     {
         isGrounded = state;
     }
 
+    public void SetWalled(bool state)
+    {
+        isWalled = state;
+    }
 
+    public void SetLeftWall(bool state)
+    {
+        isWallLeft = state;
+    }
+
+    public void SetRightWall(bool state)
+    {
+        isWallRight = state;
+    }
 
 }
